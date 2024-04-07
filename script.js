e7c6aface880f3c06cec432329ad0c854deb80ba
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-04-08 00:34:03 +0700

    menambahkan script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Haji tidak sah");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio wong saya suka kok");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Vergil");
+});
